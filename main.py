c47469dd889899a4c91fc64a3a3e206a4cf9f075
Prakharakshat <prakharverma2046@gmail.com>
2025-07-07 11:43:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,80 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+mpDraw = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+volRange = volume.GetVolumeRange()
+minVol, maxVol = volRange[0], volRange[1]
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    lmList = []
+    if results.multi_hand_landmarks:
+        handLms = results.multi_hand_landmarks[0]
+        for id, lm in enumerate(handLms.landmark):
+            h, w, _ = img.shape
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            lmList.append((cx, cy))
+        mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+        x1, y1 = lmList[4]
+        x2, y2 = lmList[8]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 10, (255, 0, 0), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 10, (255, 0, 0), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+        cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        vol = np.interp(length, [30, 200], [minVol, maxVol])
+        volume.SetMasterVolumeLevel(vol, None)
+
+        volBar = np.interp(length, [30, 200], [400, 150])
+        volPerc = np.interp(length, [30, 200], [0, 100])
+
+        cv2.rectangle(img, (50, 150), (85, 400), (0, 255, 0), 3)
+        cv2.rectangle(img, (50, int(volBar)), (85, 400), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, f'{int(volPerc)} %', (40, 430), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+
+    cv2.imshow("Volume Control", img)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
